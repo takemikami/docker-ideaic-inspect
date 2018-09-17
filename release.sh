636910eabfd2dac1902a15e03e0aa478a2cd1b46
Mikami, Takeshi <takeshi.mikami@gmail.com>
2018-09-17 22:13:45 +0900

    add circle ci setting

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 rm -rf gen/
 mkdir -p gen/
@@ -18,15 +18,17 @@ done
 
 if [ "$1" == "push" ]; then
   REPOSITORY_ROOT=$(cd $(dirname $0)/.; pwd)
+  git config --global user.name "Takeshi Mikami"
+  git config --global user.email takeshi.mikami@gmail.com
 
   for jdk in $JDK_VERSIONS; do
     cd $REPOSITORY_ROOT/gen/$jdk
     echo $REPOSITORY_ROOT/gen/$jdk
     git init
-    git remote add origin git@github.com:takemikami/docker-ideaic-inspect.git
+    git remote add origin https://${GITHUB_ACCESS_TOKEN}@github.com/takemikami/docker-ideaic-inspect
     git checkout -b $jdk
     git add --all :/
-    git commit -m "[auto] docker build branch"
+    git commit -m "update docker build branch [ci skip]"
     git push origin $jdk --force
   done
 
